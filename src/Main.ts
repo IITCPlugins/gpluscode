136c59d46dfb7d6315a19139c01fc549a530a95c
McBen <viertelvor12@gmx.net>
2026-05-31 12:26:35 +0200

    city string is always valid

--- src/Main.ts
+++ src/Main.ts
@@ -30,33 +30,32 @@ class GPlusCodes implements Plugin.Class {
             const code = term.substring(0, term.indexOf(' '));
             const city = term.substring(term.indexOf(' ') + 1); 
             if (OpenLocationCode.isShort(code)) {
-                if  (city) {
-                    var url = new URL("https://nominatim.openstreetmap.org/search?format=json");
-                    url.searchParams.set('q', city);
-                    const response = await fetch(url)
-                    if (!response.ok) {
-                        console.error("Failed to fetch city coordinates for " + city, response.statusText);
-                        return;
-                    }
-
-                    const cities = await response.json();
-                    const cityLocation = cities[0]; // just pick first match
-                    if (!cityLocation) {
-                        console.error("Failed to fetch cityLocation for " + city, cities);
-                        return;
-                    }
-
-                    const fullcode = OpenLocationCode.recoverNearest(code, parseFloat(cityLocation.lat), parseFloat(cityLocation.lon));
-                    const result = OpenLocationCode.decode(fullcode);
-                    search.addResult({
-                        title: `GPlusCode: ${term}`,
-                        description: `Lat: ${result.latitudeCenter.toFixed(6)}, Lng: ${result.longitudeCenter.toFixed(6)}`,
-                        position: {
-                            lat: result.latitudeCenter,
-                            lng: result.longitudeCenter
-                        }
-                    })
+                console.assert(city !== undefined && city !== term, "no city name?")
+                const url = new URL("https://nominatim.openstreetmap.org/search?format=json");
+                url.searchParams.set('q', city);
+                const response = await fetch(url)
+                if (!response.ok) {
+                    console.error("Failed to fetch city coordinates for " + city, response.statusText);
+                    return;
                 }
+
+                const cities = await response.json();
+                const cityLocation = cities[0]; // just pick first match
+                if (!cityLocation) {
+                    console.error("Failed to fetch cityLocation for " + city, cities);
+                    return;
+                }
+
+                const fullcode = OpenLocationCode.recoverNearest(code, parseFloat(cityLocation.lat), parseFloat(cityLocation.lon));
+                const result = OpenLocationCode.decode(fullcode);
+                search.addResult({
+                    title: `GPlusCode: ${term}`,
+                    description: `Lat: ${result.latitudeCenter.toFixed(6)}, Lng: ${result.longitudeCenter.toFixed(6)}`,
+                    position: {
+                        lat: result.latitudeCenter,
+                        lng: result.longitudeCenter
+                    }
+                })
             }
 
         }
